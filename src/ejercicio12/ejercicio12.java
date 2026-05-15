103b280c96b27bdd0898f8ee7088bf58becf65b2
unknown <you@example.com>
2026-05-15 11:30:26 -0300

    actualisar el ejercisio12

--- src/ejercicio12/ejercicio12.java
+++ src/ejercicio12/ejercicio12.java
@@ -8,6 +8,32 @@ public class ejercicio12 {
 	
 	public static void main(String[] args) {
 
+		mostrarEnPantalla(  obtenerFraccionario( ingresarNumero() )   );
+		
+	}
+
+	private static void mostrarEnPantalla( double numFracc  ) {
+		
+		System.out.println("El numero fraccionario es: " + numFracc);
+		
+	}
+
+	private static double obtenerFraccionario( double numIngr ) {
+	
+		double numFraccionario ;
+		int numEntero = (int) numIngr;
+	
+		numFraccionario = numIngr - (double) numEntero;
+		
+		return numFraccionario;
+	}
+
+	private static double ingresarNumero() {
+		System.out.println("Ingrese un número");
+		
+		double numeroIgresado = entrada.nextDouble();
+		
+		return numeroIgresado;
 	}
 	
 	
